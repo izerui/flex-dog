b827c13bcb4c07140fa51b98327336207a5bea99
serv <yifeng2000@126.com>
2018-09-30 17:40:54 +0800

    修改需求, 增加货品属性的选择

--- src/main/java/com/github/izerui/file/service/OnlineService.java
+++ src/main/java/com/github/izerui/file/service/OnlineService.java
@@ -1,24 +1,26 @@
 package com.github.izerui.file.service;
 
+import com.ecworking.audit.Record;
 import com.ecworking.rbac.dto.EntSearch;
 import com.ecworking.rbac.dto.EnterpriseEntity;
 import com.ecworking.rbac.enums.EnterpriseModelType;
 import com.ecworking.rbac.remote.vo.RoleVo;
 import com.ecworking.rbac.remote.vo.UserVo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.izerui.file.client.EnterpriseClient;
 import com.github.izerui.file.client.RoleClient;
 import com.github.izerui.file.client.UserClient;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.flex.remoting.RemotingDestination;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RemotingDestination
@@ -34,15 +36,19 @@ public class OnlineService {
     private RoleClient roleClient;
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private List<Record> userRecordListLimit100 = new ArrayList<>();
 
 
-    public List<RoleVo> findRoles(String entCode){
+    public List<RoleVo> findRoles(String entCode) {
         List<RoleVo> roles = roleClient.findRoles(entCode);
         return roles;
     }
 
-    public List<UserVo> findByRoleCode(String entCode,String roleCode){
-        return userClient.findByRoleCode(entCode,roleCode);
+    public List<UserVo> findByRoleCode(String entCode, String roleCode) {
+        return userClient.findByRoleCode(entCode, roleCode);
     }
 
     public List<Map<String, Object>> onlineUsers() throws Exception {
@@ -54,7 +60,7 @@ public class OnlineService {
         List<EnterpriseEntity> entities = enterpriseClient.data(entSearch);
 
         for (EnterpriseEntity entity : entities) {
-            if(!entity.isEnable()){
+            if (!entity.isEnable()) {
                 continue;
             }
             Map<String, Object> online = restTemplate.getForObject("http://websocket-server/online/" + entity.getCode(), Map.class);
@@ -65,19 +71,36 @@ public class OnlineService {
             map.put("type", EnterpriseModelType.valueOf(entity.getType()).getText());
             map.put("status", entity.getDeposit().getText());
             List<String> codes = userClient.getRelatedUserCodes(entity.getCode());
-            if(codes!=null){
-                map.put("relatedCount",codes.size());
-            }else{
-                map.put("relatedCount",0);
+            if (codes != null) {
+                map.put("relatedCount", codes.size());
+            } else {
+                map.put("relatedCount", 0);
             }
-            map.put("postCount",userClient.postAttachedUserCount(entity.getCode()));
+            map.put("postCount", userClient.postAttachedUserCount(entity.getCode()));
             map.put("count", online.get("在线用户数"));
-            map.put("date",entity.getCreateDate());
-            map.put("users", StringUtils.join(((List<String>) online.get("在线用户列表")).toArray(),","));
+            map.put("date", entity.getCreateDate());
+            map.put("users", StringUtils.join(((List<String>) online.get("在线用户列表")).toArray(), ","));
             results.add(map);
         }
         List<Map<String, Object>> collect = results.stream().sorted((o1, o2) -> (Integer) o2.get("count") - (Integer) o1.get("count")).collect(Collectors.toList());
         return collect;
     }
+
+
+    @RabbitListener(queues = "audit")
+    public void input(Message<byte[]> message) throws IOException {
+        Record record = objectMapper.readValue(message.getPayload(), Record.class);
+        if (userRecordListLimit100.size() > 100) {
+            userRecordListLimit100.remove(0);
+        }
+        userRecordListLimit100.add(record);
+    }
+
+    public List<Record> getUserRecordListLimit100() {
+        List<Record> records = new ArrayList<>(userRecordListLimit100);
+        Collections.reverse(records);
+        return records;
+    }
+
 
 }

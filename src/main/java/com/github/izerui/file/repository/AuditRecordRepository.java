662f5e683a8c8cfe05cf6d4c8402931adb57f73f
serv <yifeng2000@126.com>
2018-10-12 16:38:36 +0800

    修改需求, 增加货品属性的选择

---
+++ src/main/java/com/github/izerui/file/repository/AuditRecordRepository.java
@@ -0,0 +1,7 @@
+package com.github.izerui.file.repository;
+
+import com.ecworking.jpa.PlatformJpaRepository;
+import com.github.izerui.file.entity.AuditRecordEntity;
+
+public interface AuditRecordRepository extends PlatformJpaRepository<AuditRecordEntity, Long> {
+}
--- src/main/java/com/github/izerui/file/service/StService.java
+++ src/main/java/com/github/izerui/file/service/StService.java
@@ -1,9 +1,9 @@
 package com.github.izerui.file.service;
 
 import com.ecworking.audit.Record;
-import com.github.izerui.file.entity.UserStatistical;
+import com.github.izerui.file.entity.AuditRecordEntity;
+import com.github.izerui.file.repository.AuditRecordRepository;
 import com.github.izerui.file.repository.StIgnoreTypeRepository;
-import com.github.izerui.file.repository.UserStatisticalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -14,45 +14,39 @@ import org.springframework.transaction.annotation.Transactional;
 public class StService {
 
     @Autowired
-    private UserStatisticalRepository userStatisticalRepository;
+    private AuditRecordRepository auditRecordRepository;
 
     @Autowired
     private StIgnoreTypeRepository stIgnoreTypeRepository;
 
     @Async
     public void logCount(Record record) {
-        //如果在排除列表就删除同类型数据，不再记录
+        //如果在排除列表就不记录
         if (stIgnoreTypeRepository.count(record.getApplication(), record.getType(), record.getName()) > 0L) {
-            userStatisticalRepository.delete(record.getApplication(), record.getType(), record.getName());
             return;
         }
 
-        UserStatistical st;
-        long count = userStatisticalRepository.count(record.getUserCode(), record.getApplication(), record.getType(), record.getName());
-        if (count > 0L) {
-            st = userStatisticalRepository.get(record.getUserCode(), record.getApplication(), record.getType(), record.getName());
-        } else {
-            st = new UserStatistical();
-        }
-        st.setApplication(record.getApplication());
-        st.setType(record.getType());
-        st.setSignature(record.getSignature());
-        st.setName(record.getName());
-        st.setUrl(record.getUrl());
-        st.setAccountCode(record.getAccountCode());
-        st.setAccountName(record.getAccountName());
-        st.setUserCode(record.getUserCode());
-        st.setUserName(record.getUserName());
-        st.setEntCode(record.getEntCode());
-        st.setEntName(record.getEntName());
-        if (record.getName() != null && record.getName().equals("登录系统")) {
-            st.setLastLoginTime(record.getBegin());
-        }
-        st.setLastOptTime(record.getBegin());
-        st.setCount(st.getCount() + 1);
-        userStatisticalRepository.save(st);
+        AuditRecordEntity recordEntity = new AuditRecordEntity();
+        recordEntity.setBegin(record.getBegin());
+        recordEntity.setApplication(record.getApplication());
+        recordEntity.setType(record.getType());
+        recordEntity.setSignature(record.getSignature());
+        recordEntity.setName(record.getName());
+        recordEntity.setIp(record.getIp());
+        recordEntity.setUrl(record.getUrl());
+        recordEntity.setAccountCode(record.getAccountCode());
+        recordEntity.setAccountName(record.getAccountName());
+        recordEntity.setUserCode(record.getUserCode());
+        recordEntity.setUserName(record.getUserName());
+        recordEntity.setEntCode(record.getEntCode());
+        recordEntity.setEntName(record.getEntName());
+        recordEntity.setSuccess(record.isSuccess());
+        recordEntity.setEnd(record.getEnd());
+        recordEntity.setTime(record.getTime());
+        recordEntity.setAppId(record.getAppId());
+        recordEntity.setToken(record.getToken());
+        auditRecordRepository.save(recordEntity);
     }
-
 
 
 }
